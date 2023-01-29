4282cd7aed8812fa787f862d533197f448439858
Carlos Matheu <info@plastikaweb.com>
2023-01-29 09:53:03 +0100

    add UserList view

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { User } from './models/User';
-import { UserEdit } from './views/UserEdit';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+import { UserList } from './views/UserList';
 
-const user = User.build({
-  name: 'NAME', age: 20
-})
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.build(json);
+});
 
-const root = document.getElementById('root');
+users.on('change', () => {
+  const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
 
-  console.log(userEdit);
-} else {
-  throw new Error('root element not found');
-}
+users.fetch();
